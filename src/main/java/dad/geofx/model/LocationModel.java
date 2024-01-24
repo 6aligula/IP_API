0f7b70754f6660df9e2993b768e03c101d2cb616
Vinicio <calamot51@gmail.com>
2024-01-24 16:26:01 +0100

    modelo creado

---
+++ src/main/java/dad/geofx/model/LocationModel.java
@@ -0,0 +1,98 @@
+package dad.geofx.model;
+
+// LocationModel.java
+public class LocationModel {
+    private double latitude;
+    private double longitude;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private String language;
+    private String timeZone;
+    private String callingCode;
+    private String currency;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public String getCallingCode() {
+        return callingCode;
+    }
+
+    public void setCallingCode(String callingCode) {
+        this.callingCode = callingCode;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    // Constructor, getters y setters para cada variable.
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
